55441e228e799252590f973103ab13c5120439ce
gustavomansilla86 <gustavomansilla1986@hotmail.com>
2024-05-09 20:31:45 -0300

    guardar

--- saludo.py
+++ saludo.py
@@ -1,7 +1,4 @@
-#hola
-<<<<<<< HEAD
-#como estas
-print("hola mundo")
-=======
-#como estas
->>>>>>> 8d3c4aedea281fe14511acfc858b2da432df2512
+def (saludo)
+saludos= str(hola mundo)
+print(saludos);
+saludo
